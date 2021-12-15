48e83d6652598ad79a8b4afba45b031c4075c704
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-12-14 18:33:08 -0600

    Creación de componentes
    Creation of commponents folder
    
    Co-Authored-By: Ernesto2899 <50375286+Ernesto2899@users.noreply.github.com>
    Co-Authored-By: Gabriel <gabrielalejandro.br99@gmail.com>
    Co-Authored-By: Javier-Barreto <60673812+Javier-Barreto@users.noreply.github.com>

---
+++ FlightBookingApp/src/Screens/Prueba.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Conteiner} from '../Assets/styled';
+
+export const Prueba = () => {
+    return (  
+        <Conteiner>
+            
+        </Conteiner>
+    );
+  };
