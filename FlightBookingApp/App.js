7856a50252d72342edcf224beeeff87417bb61c3
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-01-06 16:28:37 -0600

    Heading style
    The title of the navigation is modified to achieve a similarity to the mockup
    
    Co-Authored-By: Ernesto2899 <50375286+Ernesto2899@users.noreply.github.com>
    Co-Authored-By: Javier Anastasio <60673812+Javier-Barreto@users.noreply.github.com>

--- FlightBookingApp/App.js
+++ FlightBookingApp/App.js
@@ -9,18 +9,10 @@ const App = () => {
   return (
   <NavigationContainer>{ 
     <Stack.Navigator>
-      <Stack.Screen name='Login' component={Login} options={{headerTintColor: '#5974f5',  headerTitleStyle: {
-            fontWeight: 'bold', 
-      }}}/>
-      <Stack.Screen name='SignUp' component={SignUp} options={{headerTintColor: '#5974f5',  headerTitleStyle: {
-            fontWeight: 'bold', 
-      }}}/>
-      <Stack.Screen name='My Flights' component={MyFlights} options={{headerTintColor: '#5974f5',  headerTitleStyle: {
-            fontWeight: 'bold',
-      }}}/>
-      <Stack.Screen name='Add Flight' component={AddFlight} options={{headerTintColor: '#5974f5',  headerTitleStyle: {
-            fontWeight: 'bold', 
-      }}}/>
+      <Stack.Screen name='Login' component={Login} options={{headerTintColor: '#5974f5', headerTitleStyle:{fontSize: 30, fontWeight: 'bold',}, headerTitleAlign:'left'}}/>
+      <Stack.Screen name='SignUp' component={SignUp} options={{headerTintColor: '#5974f5',  headerTitleStyle:{fontSize: 30, fontWeight: 'bold',}, headerTitleAlign:'left'}}/>
+      <Stack.Screen name='My Flights' component={MyFlights} options={{headerTintColor: '#5974f5', headerTitleStyle:{fontSize: 30, fontWeight: 'bold',}, headerTitleAlign:'left'}}/>
+      <Stack.Screen name='Add Flight' component={AddFlight} options={{headerTintColor: '#5974f5', headerTitleStyle:{fontSize: 30, fontWeight: 'bold',}, headerTitleAlign:'left'}}/>
     </Stack.Navigator>
   }</NavigationContainer>
   )
