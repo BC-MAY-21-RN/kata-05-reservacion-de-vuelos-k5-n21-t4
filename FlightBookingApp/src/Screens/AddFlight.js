244fb66c8b4cfa0d9cdfff9b5c8ab0f5e075b6d7
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-06 17:05:46 -0600

    Multiscreen form setup
    Setting up the multiscreen form navigation

--- FlightBookingApp/src/Screens/AddFlight.js
+++ FlightBookingApp/src/Screens/AddFlight.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {NavigationContainer } from '@react-navigation/native'
+import {Provider as PaperProvider} from 'react-native-paper'
+import { FormProvider } from './AddFlightFormScreens/AFFormContext'
+import {AddFlightNavigator as Navigator} from '../Components/AddFlightNavigator'
 
-export const AddFlight = ({navigation}) => {
+//replace the  things on top with the actual files
+
+//i stopped at 50% on the multi screen form tut
+
+export const AddFlight = () => {
     return (
+<<<<<<< HEAD
         <View>
             <Text onPress={() => navigation.navigate('Origen')}> yeh</Text>
         </View>
+=======
+        <PaperProvider>
+            <FormProvider>
+                <NavigationContainer>
+                    <Navigator />
+                </NavigationContainer>
+            </FormProvider>
+        </PaperProvider>
+>>>>>>> eed5d1b (Multiscreen form setup)
     )
 };
