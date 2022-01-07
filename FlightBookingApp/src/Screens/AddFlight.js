2ada48f0d87a0b5f317959695812ae5aefe89d2e
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-07 17:37:49 -0600

    WIP- navigator

--- FlightBookingApp/src/Screens/AddFlight.js
+++ FlightBookingApp/src/Screens/AddFlight.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import {NavigationContainer } from '@react-navigation/native'
-import {Provider as PaperProvider} from 'react-native-paper'
-import { FormProvider } from './AddFlightFormScreens/AFFormContext'
-import {AddFlightNavigator as Navigator} from '../Components/AddFlightNavigator'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {AFFormContext as FormProvider} from './AddFlightFormScreens/AFFormContext';
+import {AddFlightNavigator as Navigator} from '../Components/AddFlightNavigator';
 
-//replace the  things on top with the actual files
-
-//i stopped at 50% on the multi screen form tut
-
-export const AddFlight = () => {
-    return (
-        <PaperProvider>
-            <FormProvider>
-                <NavigationContainer>
-                    <Navigator />
-                </NavigationContainer>
-            </FormProvider>
-        </PaperProvider>
-    )
+export const AddFlight = ({navigation}) => {
+  return (
+    <PaperProvider>
+      <FormProvider>
+        <NavigationContainer>
+          <Navigator />
+        </NavigationContainer>
+      </FormProvider>
+    </PaperProvider>
+  );
 };
