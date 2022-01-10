bc030c405a19c7c9fdc6660d32b8181cd91118b3
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-01-10 11:15:57 -0600

    get flight information
    se opbtiene la informacion mediante querys de firebase, para poder desplegar los vuelos de los usuarios

--- FlightBookingApp/src/Components/CardFlight/My_flights_list.js
+++ FlightBookingApp/src/Components/CardFlight/My_flights_list.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import {FlList} from '../../Assets/cardStyled'
-import {  CardFlight } from './index'
+import { CardFlight } from './index'
 import {flight} from '../../Assets/exampleInfo.json';
-//import db from '../firebase/config';
+import { getFlights } from '../../Assets/hooks/firebase/loadData';
+import { getFlightsList } from '../../Assets/hooks/firebase/infoVuelos';
 
 export const MyFlights_List = () => {
+  const [data, setData] = useState()
+  
+  async function loadData(){
+    try{
+      const response = await getFlights('Dylanlegendary1@gmail.com')
+      const info = await getFlightsList(response)
+      console.log(info);       
+    } catch(e){
+      console.log('Este es un error '+ e)
+    }
+  }
+
+  useEffect(()=>{
+    loadData()
+  }, []);
 
   const renderPlace = ({item})=>{
     return(
