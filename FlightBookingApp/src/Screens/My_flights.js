7b665d6db4e683022e28b63dae11c218b3b6cd3f
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-11 17:24:16 -0600

    Update My_flights.js

--- FlightBookingApp/src/Screens/My_flights.js
+++ FlightBookingApp/src/Screens/My_flights.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {ScrlView, Container2} from '../Assets/cardStyled'
-import { MyFlights_List, PlusButton } from '../Components/CardFlight';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
+import {Container2} from '../Assets/cardStyled'
+import { PlussButton, MyFlights_List } from '../Components/CardFlight/index';
+//import db from '../firebase/config';
 
 const My_flights = () => {
   return (
-      <Container2>
-        {/* <MyFlights_List/> */}
-        <PlusButton onPress={() => navigation.navigate('Origen')}/>
-      </Container2>
-  )
-}
-
-export default My_flights
+    <Container2>
+        <MyFlights_List/>
+        <PlussButton onPress={() => navigation.navigate('Add Flight')}/>
+    </Container2>
+  );
+};
