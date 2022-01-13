470b196cd09546dd8e561c95e0a9715ee800a20c
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-12 19:35:19 -0600

    Project structure cleanup
    removed some unused files and folders

--- FlightBookingApp/src/Screens/index.js
+++ FlightBookingApp/src/Screens/index.js
@@ -1,7 +1,6 @@
 export {SignUp} from "./SignUp";
 export {Login} from "./Login";
 export {MyFlights} from "./MyFlights";
-export {AddFlight} from "./AddFlight";
 export {Origen} from '../Components/MyFlightComponents/Origen'
 export {Destino} from '../Components/MyFlightComponents/Destino'
 export {Fecha} from '../Components/MyFlightComponents/Fecha'
